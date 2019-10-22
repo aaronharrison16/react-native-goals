2c74a0c8c49af8fb3fb240e19fd5c3e960de109b
Aaron Harrison <aaronharrison16@gmail.com>
2019-10-21 22:44:16 -0600

    Learned about and created the stylesheet object

--- App.js
+++ App.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={{padding: 30}}>
-      <View>
-        <TextInput placeholder="Course Goal" style={{borderBottomColor: 'black', borderBottomWidth: 1, padding: 10 }} />
-        <Button title='Add'/>
-      </View>
-      
-      <View>
-
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal" style={styles.input} />
+        <Button title="Add" />
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 60
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  input: {
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    padding: 10,
+    width: 200
+  }
+});
